c0d61d699618bae909601d85ec8358b57f6d7bf4
Salomé Cheysson <salome@cheysson.fr>
2025-01-15 10:25:56 +0100

    extraire liste adresses députés

--- src/hexagonal/assemblee_nationale/liste.py
+++ src/hexagonal/assemblee_nationale/liste.py
@@ -5,7 +5,11 @@ from zipfile import ZipFile
 import click
 from glom import Coalesce, Invoke, T, glom
 
-from hexagonal.assemblee_nationale.utils import json_personnes, possiblement_nul
+from hexagonal.assemblee_nationale.utils import (
+    json_personnes,
+    possiblement_nul,
+    to_list,
+)
 
 PCS_FAM = {
     "Agriculteurs exploitants": "1",
@@ -56,6 +60,23 @@ def legislatures_depute(depute):
     )
 
 
+TYPES_ADRESSES = {
+    0: "adresse_officielle",
+    1: "adresse_parisienne",
+    2: "adresse_circonscription",
+    3: "telephone_presse",
+    11: "telephone",
+    12: "fax",
+    15: "email",
+    22: "site_web",
+    23: "url_senateur",
+    24: "twitter",
+    25: "facebook",
+    29: "instagram",
+    30: "linkedin",
+}
+
+
 spec_extraction_liste_deputee = {
     "id_personne": Coalesce("uid.#text", "uid"),
     "civilite": "etatCivil.ident.civ",
@@ -79,10 +100,51 @@ spec_extraction_liste_deputee = {
 }
 
 
-def extraire_liste_deputes(archive, fiches, deputes):
+spec_adresse_postale = {
+    "id_adresse": Coalesce("uid.#text", "uid"),
+    "id_personne": Coalesce("id_personne.#text", "id_personne"),
+    "type": ("type", int, TYPES_ADRESSES.get),
+    "intitule": Coalesce("intitule", default=""),
+    "numero_rue": Coalesce("numeroRue", default=""),
+    "code_postal": Coalesce("codePostal", default=""),
+    "ville": Coalesce("ville", default=""),
+    "complement": Coalesce("complementAdresse", default=""),
+    "nom_rue": Coalesce("nomRue", default=""),
+    "poids": Coalesce("poids", default=""),
+}
+
+spec_adresse_electronique = {
+    "id_adresse": Coalesce("uid.#text", "uid"),
+    "id_personne": Coalesce("id_personne.#text", "id_personne"),
+    "type": ("type", int, TYPES_ADRESSES.get),
+    "adresse_rattachement": Coalesce("adresseDeRattachement", default=""),
+    "adresse": Coalesce("valElec", default=""),
+    "poids": Coalesce("poids", default=""),
+}
+
+
+def extraire_liste_deputes(
+    archive, fiches, deputes, adresses_postales, adresses_electroniques
+):
     personnes = sorted(
         glom(json_personnes(archive), [spec_extraction_liste_deputee]),
         key=itemgetter("id_personne"),
+    )
+
+    liste_adresses = [
+        {"id_personne": d["uid"], **a}
+        for d in json_personnes(archive)
+        if "@xsi:nil" not in d["adresses"]
+        for a in to_list(d["adresses"]["adresse"])
+    ]
+
+    liste_adresses_postales = glom(
+        [a for a in liste_adresses if int(a["type"]) <= 10],
+        [spec_adresse_postale],
+    )
+    liste_adresses_electroniques = glom(
+        [a for a in liste_adresses if int(a["type"]) > 10],
+        [spec_adresse_electronique],
     )
 
     fiches_writer = csv.DictWriter(
@@ -97,6 +159,18 @@ def extraire_liste_deputes(archive, fiches, deputes):
     deputes_writer.writeheader()
     deputes_writer.writerows(p for p in personnes if p["legislatures"])
 
+    adresses_postales_writer = csv.DictWriter(
+        adresses_postales, fieldnames=list(spec_adresse_postale)
+    )
+    adresses_postales_writer.writeheader()
+    adresses_postales_writer.writerows(liste_adresses_postales)
+
+    adresses_electroniques_writer = csv.DictWriter(
+        adresses_electroniques, fieldnames=list(spec_adresse_electronique)
+    )
+    adresses_electroniques_writer.writeheader()
+    adresses_electroniques_writer.writerows(liste_adresses_electroniques)
+
 
 @click.command()
 @click.argument(
@@ -104,9 +178,13 @@ def extraire_liste_deputes(archive, fiches, deputes):
 )
 @click.argument("fiches", type=click.File("w"))
 @click.argument("deputes", type=click.File("w"))
-def run(archive_path, fiches, deputes):
+@click.argument("adresses_postales", type=click.File("w"))
+@click.argument("adresses_electroniques", type=click.File("w"))
+def run(archive_path, fiches, deputes, adresses_postales, adresses_electroniques):
     with ZipFile(archive_path) as archive:
-        extraire_liste_deputes(archive, fiches, deputes)
+        extraire_liste_deputes(
+            archive, fiches, deputes, adresses_postales, adresses_electroniques
+        )
 
 
 if __name__ == "__main__":
